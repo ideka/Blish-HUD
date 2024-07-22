b9167714817b2cd398b94eabad63b4bc0759c3fc
Gerardo Marset <gammer1994@gmail.com>
2024-07-22 02:44:32 +0100

    Try to delete LockFile on dispose

--- Blish HUD/GameServices/LocalDb/LockFile.cs
+++ Blish HUD/GameServices/LocalDb/LockFile.cs
@@ -5,15 +5,17 @@ using System.IO;
 
 namespace Blish_HUD.LocalDb {
     internal class LockFile : IDisposable {
+        private readonly string _path;
         private readonly FileStream _stream;
 
-        private LockFile(FileStream stream) {
-            _stream = stream;
+        private LockFile(string path) {
+            _path = path;
+            _stream = File.Open(_path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
         }
 
         public static LockFile? TryAcquire(string path) {
             try {
-                return new LockFile(File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None));
+                return new LockFile(path);
             } catch (IOException e) when (e.GetType() == typeof(IOException)) {
                 return null;
             }
@@ -21,6 +23,11 @@ namespace Blish_HUD.LocalDb {
 
         public void Dispose() {
             _stream.Dispose();
+            try {
+                File.Delete(_path);
+            } catch {
+                // Ignore
+            }
         }
     }
 }
